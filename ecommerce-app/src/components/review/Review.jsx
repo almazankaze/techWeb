e046d63a2aa8c544de194f5acadc23cc9b024006
John Almazan <almazankaze@live.com>
2023-03-07 19:02:05 -0600

    finished product page

--- ecommerce-app/src/components/review/Review.jsx
+++ ecommerce-app/src/components/review/Review.jsx
@@ -7,9 +7,8 @@ import userIcon from "../../img/default-user.png";
 import "./review.css";
 
 function Review() {
-  
   const currentUser = useSelector(selectCurrentUser);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -18,44 +17,49 @@ function Review() {
     <div className="product-review">
       <ReviewDetails />
 
-      <form
-        autoComplete="off"
-        noValidate
-        onSubmit={handleSubmit}
-        className="my-form"
-      >
-        <div className="star-container">
-          <h3>Your Rating: </h3>
-          <div className="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label htmlFor="star5" title="5/5">
-              5 stars
-            </label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label htmlFor="star4" title="4/5">
-              4 stars
-            </label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label htmlFor="star3" title="3/5">
-              3 stars
-            </label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label htmlFor="star2" title="2/5">
-              2 stars
-            </label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label htmlFor="star1" title="1/5">
-              1 star
-            </label>
+      {currentUser ? (
+        <form
+          autoComplete="off"
+          noValidate
+          onSubmit={handleSubmit}
+          className="my-form"
+        >
+          <div className="star-container">
+            <h3>Your Rating: </h3>
+            <div className="rate">
+              <input type="radio" id="star5" name="rate" value="5" />
+              <label htmlFor="star5" title="5/5">
+                5 stars
+              </label>
+              <input type="radio" id="star4" name="rate" value="4" />
+              <label htmlFor="star4" title="4/5">
+                4 stars
+              </label>
+              <input type="radio" id="star3" name="rate" value="3" />
+              <label htmlFor="star3" title="3/5">
+                3 stars
+              </label>
+              <input type="radio" id="star2" name="rate" value="2" />
+              <label htmlFor="star2" title="2/5">
+                2 stars
+              </label>
+              <input type="radio" id="star1" name="rate" value="1" />
+              <label htmlFor="star1" title="1/5">
+                1 star
+              </label>
+            </div>
           </div>
-        </div>
-        <textarea
-          className="comment-box"
-          placeholder="Add a comment..."
-        ></textarea>
+          <textarea
+            className="comment-box"
+            placeholder="Add a comment..."
+          ></textarea>
 
-        <Button type="submit">Add Review</Button>
-      </form>
+          <Button type="submit">Add Review</Button>
+        </form>
+      ) : (
+        ""
+      )}
+
       <div className="comment-section">
         <h3>REVIEWS</h3>
         <div className="comments-section">
